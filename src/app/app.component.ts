1036e10ed2df688d3ff3cab43e09202c28538c30
jpzafra <jpzafra@gmail.com>
2022-03-24 19:56:28 +0100

    ejercicio resuelto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  /*
+
+¿ES UN PALÍNDROMO?
+
+Fecha publicación enunciado: 21/03/22
+Fecha publicación resolución: 28/03/22
+Dificultad: MEDIA
+
+Enunciado: Escribe una función que reciba un texto y retorne verdadero o falso (Boolean) según sean o no palíndromos.
+Un Palíndromo es una palabra o expresión que es igual si se lee de izquierda a derecha que de derecha a izquierda.
+NO se tienen en cuenta los espacios, signos de puntuación y tildes.
+Ejemplo: Ana lleva al oso la avellana.
+
+Información adicional:
+- Usa el canal de nuestro discord (https://mouredev.com/discord) "🔁reto-semanal" para preguntas, dudas o prestar ayuda a la comunidad.
+- Puedes hacer un Fork del repo y una Pull Request al repo original para que veamos tu solución aportada.
+- Revisaré el ejercicio en directo desde Twitch el lunes siguiente al de su publicación.
+- Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
+
+  */
+
   title = 'ChkPalindromos';
+
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
+  initForm = {
+    string2chk: ''
+  }
+
+  chkPalindromo(){
+    console.log('clicked Go')
+    let output = this.initForm.string2chk;
+    output = output.trim();
+    output = output.replace(/\s+/g, '')  
+    output = output.normalize('NFD').replace(/[\u0300-\u036f]/g,"");
+    output = output.replace(/[^\w\-]+/g, '');
+    output = output.toLowerCase()
+
+    if (output===output.split("").reverse().join("")){
+      console.log(this.initForm.string2chk + ' es un palindromo.')
+    }
+  }
+
+  reset(){
+    console.log('reset clicked', this.initForm)
+    this.miFormulario.resetForm();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
